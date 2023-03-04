f651c9d62e3aef6749cc3552534e485b07df6e04
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-03-04 20:15:15 +0100

    206. solved

---
+++ src/LCproblems/SingliLinkedList/206. Reverse Linked List/reverseLinkedList.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "@jest/globals";
+import { makeLinkedList } from "../../../utils/LinkedLists";
+import { reverseLinkedList } from "./reverseLinkedList";
+
+describe("reverseLinkedList", function () {
+  it("test 1", function () {
+    const list1 = makeLinkedList([1, 2, 3, 4, 5]);
+    const res = makeLinkedList([5, 4, 3, 2, 1]);
+
+    expect(reverseLinkedList(list1)).toEqual(res);
+  });
+});
---
+++ src/LCproblems/SingliLinkedList/206. Reverse Linked List/reverseLinkedList.ts
@@ -0,0 +1,18 @@
+import { ListNode, makeLinkedList } from "../../../utils/LinkedLists";
+
+export function reverseLinkedList(head: ListNode | null): ListNode | null {
+  let newHead: ListNode | null = null;
+
+  while (head) {
+    const nextEl = head?.next || null;
+    head.next = newHead;
+    newHead = head;
+
+    head = nextEl;
+  }
+  return newHead;
+}
+
+const list1 = makeLinkedList([1, 2, 3, 4, 5]);
+
+reverseLinkedList(list1);
