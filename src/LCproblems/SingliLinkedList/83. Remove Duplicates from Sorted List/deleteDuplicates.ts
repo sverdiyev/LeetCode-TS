ef3695e1eeddad9e899b917f8951978d726bfa54
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-03-04 21:43:48 +0100

    83. solved

---
+++ src/LCproblems/SingliLinkedList/83. Remove Duplicates from Sorted List/deleteDuplicates.ts
@@ -0,0 +1,17 @@
+import { ListNode, makeLinkedList } from "../../../utils/LinkedLists";
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  const originalHead = head;
+
+  while (head) {
+    if (head.val === head.next?.val) {
+      head.next = head.next.next;
+    } else {
+      head = head.next;
+    }
+  }
+  return originalHead;
+}
+const list = makeLinkedList([1, 1, 2, 3, 3]);
+
+deleteDuplicates(list);
