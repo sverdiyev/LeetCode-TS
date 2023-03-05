4ce16a1993ba8853ea9782ea0933cb3ce9c56f0a
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-03-05 22:13:53 +0100

    141. added better solution

--- src/LCproblems/SingliLinkedList/141. Linked List Cycle/hasCycle.ts
+++ src/LCproblems/SingliLinkedList/141. Linked List Cycle/hasCycle.ts
@@ -1,5 +1,11 @@
 import { ListNode } from "../../../utils/LinkedLists";
 
+/**
+ * Time o(N)
+ *
+ * Space O(n) cuz creating hashmap with all values
+ *
+ * */
 function hasCycle(head: ListNode | null): boolean {
   const map = new Map();
 
@@ -11,5 +17,28 @@ function hasCycle(head: ListNode | null): boolean {
     head = head.next;
   }
 
+  return false;
+}
+
+/**
+ * {@link https://www.youtube.com/watch?v=gBTe7lFR3vc&ab_channel=NeetCode}
+ *
+ *
+ * Time o(N)
+ *
+ * Space O(1) cuz NOT creating hashmap with all values
+ *
+ * */
+
+function hasCyclePointers(head: ListNode | null): boolean {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow?.next || null;
+    fast = fast?.next?.next || null;
+
+    if (fast == slow) return true;
+  }
   return false;
 }
